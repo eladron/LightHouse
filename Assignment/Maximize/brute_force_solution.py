0f03deaba9b2acee8b3ac786187cf0c875096ee5
eladron <elad.ron@campus.technion.ac.il>
2023-05-04 19:07:15 +0300

    change the brute force solution to LP solver

---
+++ Assignment/Maximize/brute_force_solution.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import itertools
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python maximize_productivity.py  <input_file>")
+    exit(1)
+
+df = pd.read_excel(sys.argv[1], index_col=0)
+
+# Get the number of stations of each type
+station_counts = df.iloc[0, :].tolist()
+print(f"Station counts: {station_counts}")
+
+# Remove the first row (station counts) and transpose the data frame
+df = df.iloc[1:]
+
+stations = list(df.columns)
+print(f"Stations: {stations}")
+# Create a list of worker names
+workers = list(df.index)
+print(f"Workers: {workers}")
+
+if len(workers) != sum(station_counts):
+    print("The number of workers should be equal to the number of stations.")
+    exit(1)
+
+# Create a list of station types
+station_types = []
+for i, count in enumerate(station_counts):
+    station_types.extend([i] * count)
+# Generate all possible assignments of workers to stations
+valid_assignments = []
+for assignment in itertools.permutations(station_types):
+    # Check if the assignment is valid
+    is_valid = True
+    for i, count in enumerate(station_counts):
+        if assignment.count(i) > count:
+            is_valid = False
+            break
+    if is_valid:
+        valid_assignments.append(assignment)
+
+print(f"Number of valid assignments: {len(valid_assignments)}")
+
+best_assignment = None
+best_score = 0
+
+for i, assignment in enumerate(valid_assignments):
+    score = sum(df.loc[worker, station+1] for worker, station in zip(workers, assignment))
+    if score > best_score:
+        best_score = score
+        best_assignment = assignment
+
+print("Best assignment:")
+for j, worker in enumerate(workers):
+    station_type = best_assignment[j]
+    print(f"{worker}: station {station_type+1}")
+print(f"Score: {best_score}")
--- Assignment/Maximize/maximize_productivity.py
+++ Assignment/Maximize/maximize_productivity.py
@@ -1,3 +1,4 @@
+import pulp
 import pandas as pd
 import itertools
 import sys
@@ -6,52 +7,50 @@ if len(sys.argv) != 2:
     print("Usage: python maximize_productivity.py  <input_file>")
     exit(1)
 
+# define the problem
+problem = pulp.LpProblem("Worker Station Assignment", pulp.LpMaximize)
+
 df = pd.read_excel(sys.argv[1], index_col=0)
+df.replace("- ∞",-1000000, inplace=True)
+for i in range(1, 10):
+    df.replace(f"{i}", i, inplace=True)
 
 # Get the number of stations of each type
 station_counts = df.iloc[0, :].tolist()
 print(f"Station counts: {station_counts}")
-
-# Remove the first row (station counts) and transpose the data frame
 df = df.iloc[1:]
 
-stations = list(df.columns)
-print(f"Stations: {stations}")
-# Create a list of worker names
-workers = list(df.index)
-print(f"Workers: {workers}")
+# create the decision variables
+workers_names = list(df.index)
+workers = range(len(workers_names))
+print(f"Workers: {workers_names}")
+stations_names = list(df.columns)
+stations = range(len(stations_names))
+print(f"Stations: {stations_names}")
+assign = pulp.LpVariable.dicts("Assign", (workers, stations), lowBound=0, upBound=1, cat=pulp.LpInteger)
 
-if len(workers) != sum(station_counts):
-    print("The number of workers should be equal to the number of stations.")
-    exit(1)
+# define the objective function to maximize the total grade
+grades = [list(df.loc[worker]) for worker in workers_names]
+print(grades)
 
-# Create a list of station types
-station_types = []
-for i, count in enumerate(station_counts):
-    station_types.extend([i] * count)
-# Generate all possible assignments of workers to stations
-valid_assignments = []
-for assignment in itertools.permutations(station_types):
-    # Check if the assignment is valid
-    is_valid = True
-    for i, count in enumerate(station_counts):
-        if assignment.count(i) > count:
-            is_valid = False
-            break
-    if is_valid:
-        valid_assignments.append(assignment)
+objective = pulp.lpSum(grades[w][s] * assign[w][s] for w in workers for s in stations)
+problem += objective
 
-best_assignment = None
-best_score = 0
+# add the constraints
+for w in workers:
+    problem += pulp.lpSum(assign[w][s] for s in stations) == 1
+for s in stations:
+    problem += pulp.lpSum(assign[w][s] for w in workers) == station_counts[s]
 
-for i, assignment in enumerate(valid_assignments):
-    score = sum(df.loc[worker, station+1] for worker, station in zip(workers, assignment))
-    if score > best_score:
-        best_score = score
-        best_assignment = assignment
+# solve the problem
+problem.solve()
 
-print("Best assignment:")
-for j, worker in enumerate(workers):
-    station_type = best_assignment[j]
-    print(f"{worker}: station {station_type+1}")
-print(f"Score: {best_score}")
+# print the solution
+if problem.status == pulp.LpStatusOptimal:
+    print("Maximum grade achieved:", pulp.value(problem.objective))
+    for w in workers:
+        for s in stations:
+            if pulp.value(assign[w][s]) == 1:
+                print("Worker", workers_names[w], "assigned to Station", stations_names[s])
+else:
+    print("No optimal solution found.")
