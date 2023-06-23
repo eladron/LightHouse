374924380cbf1a9448b4abe2a92c1afb32b05a3f
OrKeret17 <orkeretwin@gmail.com>
2023-06-23 15:32:59 +0300

    added fields to global kson data

--- Website-frontend/website/src/utils.ts
+++ Website-frontend/website/src/utils.ts
@@ -5,5 +5,10 @@ export const API_URL = 'https://132.69.8.35:443';
 
 declare global {
     var placements : object;
+    var product_screws : object;
+    var product_water : object;
+    var product_piston : object;
+    var product_handle : object;
+    var revenue : object;
 }
 
