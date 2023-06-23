23e3592b7f97fd75a448f2d95885c69c284fdbbd
eladron <63602693+eladron@users.noreply.github.com>
2023-06-23 15:40:53 +0300

    Add files via upload

--- Website-frontend/website/src/utils.ts
+++ Website-frontend/website/src/utils.ts
@@ -5,10 +5,10 @@ export const API_URL = 'https://132.69.8.35:443';
 
 declare global {
     var placements : object;
-    var product_screws : Number;
-    var product_water : Number;
-    var product_piston : Number;
-    var product_handle : Number;
     var revenue : String;
+    var product_screws: Number;
+    var product_handle: Number;
+    var product_water: Number;
+    var product_piston: Number;
 }
 
